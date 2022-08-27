8cc3205ff1712244a0b853f377181e958e5e7a1e
Mesut Cetin <102237861+m-cetin@users.noreply.github.com>
2022-08-27 18:36:54 +0200

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,14 +1,13 @@
 <?php
-mysql_connect("127.0.0.1","root","your_password_here");
-mysql_select_db("simplesqli");
+$var = mysqli_connect("localhost","user","YourPassword123","simplesqli");
 $user=$_POST["UserId"];
 $pass=$_POST["Password"];
-$query=mysql_query("Select * from users where username='$user' and password='$pass';");
-if(mysql_num_rows($query)){
+$query=mysqli_query($var, "Select * from users where username='$user' and password='$pass';");
+if(mysqli_num_rows($query)){
 	echo "<strong style='color:green;'>Bingo!!! You are logged in as <span style='color:red;'>admin</span> </strong><br><br>";
-	$users_data = mysql_query("select * from users;");
+	$users_data = mysqli_query("select * from users;");
 	echo "<table border='1'><thead><td>username</td><Td>password</td></thead>";
-	while($row = mysql_fetch_array($users_data)) {
+	while($row = mysqli_fetch_array($users_data)) {
 		echo "<tr><td>".$row["username"]."</td><td>".$row["password"]."</td></tr>";
 	}
 	echo "</table>";
